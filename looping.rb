f3943251c19843da622234b873914ee48f3c85d5
Mohammed Noori <kmanoorib@gmail.com>
2018-05-20 08:07:49 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def looping
 	#your code here
+	loop do
+	  puts "It's your first year at Hogwarts and you're having a tough time getting the hang of that levitation spell. Fill out the content of the method `looping` to contain a `loop` that `puts` the levitation charm `"Wingardium Leviosa"` an infinite number of times."
+	end
 end
 
 #call your method here
+looping
